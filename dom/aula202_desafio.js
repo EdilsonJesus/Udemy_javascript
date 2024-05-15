7d29be9725ad5515c12030664362214557302ea6
Edilson Jesus <edilsondevelop@gmail.com>
2024-05-14 21:02:33 -0300

    Desafio da Seção DOm, Concluído

--- dom/aula202_desafio.js
+++ dom/aula202_desafio.js
@@ -1,4 +1,28 @@
 function evento(){
-  document.getElementById('campo').styleBackground = '#ffff1a'
+  document.getElementById('campo').style.backgroundColor = 'yellow';
+  // document.getElementById('campo').style.backgroundColor= '';
+
 }
-console.log('Oi')
+
+// Minha solução 
+ // function altera(){
+ //  var campo = document.getElementById("campo");
+ //  var conteudo = campo.value.trim();
+ //  var tamanho = conteudo.length;
+ //  if (tamanho  < 3){
+ //    campo.style.backgroundColor = 'red'
+ //  } else{
+ //    campo.style.backgroundColor = 'green'
+ //  }
+ // }
+
+// Solução do professor
+function altera() {
+  var valorCampo = document.getElementById('campo').value;
+
+  if(valorCampo.length < 3 ){
+    document.getElementById('campo').style.background = "red";
+  } else {
+    document.getElementById('campo').style.background = "green"
+  }
+}
