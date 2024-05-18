2fd86e5705616cb6056fc0cfc1867a66e70c8f44
Edilson Jesus <edilsondevelop@gmail.com>
2024-05-18 08:37:08 -0300

    Falta concluir a lógica

--- Projeto1_App_Calculadora/app.js
+++ Projeto1_App_Calculadora/app.js
@@ -1,3 +1,29 @@
  function calcular(tipo, valor){
    console.log(tipo, valor)
+
+   //1° verificar seestamos recebemos tipo ou valor
+   if(tipo === 'acao'){
+     //4 ativar o botão clear, limppando o campo resultado
+
+
+     //5° testando qual operação matemática estamos recebendo
+     if(valor === '+' ||){
+       // caso qualquer uma desças condições forem atendidas, selecionamos
+       // o campo resulto e comcatenamos com valor recebido
+
+       //6°criar ação para o campo igual
+       if(valor === '=') {
+         //temos que criar esse ação em uma varável Dica, usar a função eval
+
+         //7 por fim recuperamos o valor do campo resulta
+       }
+     }
+   }else if(tipo === 'valor'){
+     //2° acesssar o campo resultador, 3° acessar o valor do atributo do campo
+     document.getElementById('resultado').value += valor;
+     //3 criar uma concatenação com o valor recebido do campo
+   }
+
+
+
  }
