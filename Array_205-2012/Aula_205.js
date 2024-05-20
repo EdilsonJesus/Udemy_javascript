44633d848a77fc0cfb37b65be94203f606bba7ab
EdilsonJesus <edilsondevelop@gmail.com>
2024-05-20 20:31:23 -0300

    Arrays

---
+++ Array_205-2012/Aula_205.js
@@ -0,0 +1,13 @@
+// com Variaáveis
+var variavel_1 = 'Banana';
+var variavel_2 = 'Maçã'
+var variavel_3 = 'Morango'
+var variavel_4 = 'Uva'
+
+//Com um único array
+
+var lista_frutas = Array();
+lista_frutas[1] = 'Banana';
+lista_frutas[2] = 'Maçã'
+lista_frutas[3] = 'Morango'
+lista_frutas[4] = 'Uva'
